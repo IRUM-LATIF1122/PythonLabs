dbcfbd455ab5cab0b3af3c3a014c87d66c185797
IRUM LATIF <razamirza00876@gmail.com>
2025-03-12 10:46:19 -0700

    update in lab1

--- lab1.py
+++ lab1.py
@@ -110,8 +110,97 @@ y = False
 print(y)
 print(type(y))
 
-# Example of boolean in condition
+#  Example of boolean in condition
 years_of_experience = 2
 is_eligible = years_of_experience > 1
 print("Is the candidate eligible?", is_eligible)
 print(type(is_eligible))
+
+#   String indices and accessing string elements
+#   Strings are arrays of characters and elements of an array can be accessed using indexing.
+#   Indices start with 0 from left side and -1 when starting from right side.
+
+string1 ="PYTHON TUTORIAL"
+print("length of string is : ", len(string1))
+string1 ="PYTHON TUTORIAL"
+print(string1[0])   # output:    P    (print 1st character)
+
+string1 ="PYTHON TUTORIAL"
+print(string1[13])   # output:    A
+
+string1 ="PYTHON TUTORIAL"
+print(string1[-1])   # output:    L   (print last character)
+
+string1 ="PYTHON TUTORIAL"
+print(string1[14])   # output:    L   (print last character)
+
+string1 ="PYTHON TUTORIAL"
+print(string1[-15])   # output:    P   (print 1st character)
+
+#   string1 ="PYTHON TUTORIAL"
+#   print(string1[18])   # output:    IndexError string index out of range
+
+#   STRING SLICING
+#   In Python, string slicing is a way to extract a portion (substring) of a string using the syntax: string[start:end]
+#   Key Points About String Slicing:
+#   starting index (INclusive)
+#   ending index  (Exclusive)
+
+print("\n From here string slicing concept starts: ")
+
+text = "PythonProgramming"
+print(text[3:7])  # Output: "honP"
+
+#   we can also use negative indices for slicing
+text = "PythonProgramming"
+print(text[-7:-1])   # Output: "rammin"
+
+#   Omitting Indices:
+#   Start omitted: Starts from the beginning.
+#   End omitted: Goes to the end of the string.
+
+text = "PythonProgramming"
+print(text[:6])  # Output: "Python"
+print(text[6:])  # Output: "Programming"
+
+
+#   Lists
+# allows you to store a collection of items, which can be of any data type (integers, strings, floats,other lists).
+# mutuable, allow duplicates, ordered
+
+print (" \n From here Lists concept started :")
+# A list of integers
+numbers = [1, 2, 3, 4, 5]
+
+# A list of strings
+fruits = ["apple", "banana", "cherry"]
+
+# A mixed data type list
+mixed = [1, "hello", 3.14, True]
+
+# A nested list (list within a list)
+nested = [1, [2, 3], [4, 5]]
+
+# Empty list
+empty_list = []
+
+
+print(numbers)
+print(fruits)
+print(mixed)
+print(nested)
+print(empty_list)
+
+
+#   LIST Slicing  (is same as string slicing)
+
+print('List Slicing Concept: ')
+numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+print(numbers[0:3])  # Output: [10, 20, 30]
+print(numbers[2:6])  # Output: [30, 40, 50, 60]
+print(numbers[:4])   # Output: [10, 20, 30, 40]
+print(numbers[4:])   # Output: [50, 60, 70, 80]
+print(numbers[-4:-1])  # Output: [50, 60, 70]
+
+# Reverse the List with Step -1
+print("Reverse list: ",numbers[::-1])   # Output: [80, 70, 60, 50, 40, 30, 20, 10]
